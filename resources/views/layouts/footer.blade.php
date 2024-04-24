930bfaaa46ec2cb4acd4301fa103fdf2935f70e2
joycelineoct <joycelineoctavia@gmail.com>
2024-04-24 14:10:21 +0700

    Tugas 3

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="footer footer-center p-4 bg-base-300 text-base-content fixed bottom-0">
+<footer class="footer footer-center p-4 bg-base-300 text-base-content" style="position: fixed; bottom:0; width: 100%">
     <aside>
       <p>Copyright © 2024 - All right reserved by Joyceline Octavia</p>
     </aside>
-  </footer>
+  </footer> 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\TodoTaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [TodoTaskController::class, 'index']);
+
+Route::post('/', [ TodoTaskController::class, 'store']);
+// Route::post('/', function () {
+//     return 'ini untuk post';
+// });
+Route::delete('/{id}', [TodoTaskController::class, 'destroy']);
